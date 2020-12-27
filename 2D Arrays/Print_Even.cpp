e4f123ed20c7ee3bced343cefc80d8e63505d33b
SubhayanDas08 <nips.subhayan@gmail.com>
2020-12-28 00:51:34 +0530

    28 Dec 2020

---
+++ 2D Arrays/Print_Even.cpp
@@ -0,0 +1,44 @@
+//Given a 2D array, print the even numbers of it.
+
+/*Enter your code here. Read input from STDIN. Print your output to STDOUT*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int count, count2;
+
+    cin >> count >> count2;
+    if (count > 0)
+    {
+        int **arr = new int*[count];
+        
+        for (int i = 0; i < count; i++)
+        arr[i] = new int[count2];
+ 
+        for (int i = 0; i < count; i++)
+            for (int j = 0; j < count2; j++)
+                cin>>arr[i][j];
+
+        for (int i = 0; i < count; i++)
+        {
+            for (int j = 0; j < count2; j++)
+            {
+                if (arr[i][j]%2==0)
+                {
+                    cout<<arr[i][j]<<" ";
+                }
+                
+            }
+            
+        }
+        
+ 
+        for (int i = 0; i < count; i++)
+            delete[] arr[i];
+ 
+        delete[] arr;
+    }
+
+    return 0;
+}

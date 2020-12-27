05ef29775665a05c7ebd125c5807941a10966aa7
SubhayanDas08 <nips.subhayan@gmail.com>
2020-12-28 00:49:28 +0530

    28 Dec 2020

---
+++ Multidimensional Arrays/2D_Array_Matrix_in_diagonal_Order.cpp
@@ -0,0 +1,35 @@
+//Given a 2D matrix, print all elements of the given matrix in diagonal order. 
+
+/*Enter your code here. Read input from STDIN. Print your output to STDOUT*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int count, count2;
+
+    cin >> count >> count2;
+    if (count > 0)
+    {
+        int **arr = new int*[count];
+        
+        for (int i = 0; i < count; i++)
+        arr[i] = new int[count2];
+ 
+        for (int i = 0; i < count; i++)
+            for (int j = 0; j < count2; j++)
+                cin>>arr[i][j];
+
+        for (int i = 0, j = 0; i < count, j < count2; i++, j++)
+        {
+            cout<<arr[i][j];
+        }
+        
+        for (int i = 0; i < count; i++)
+            delete[] arr[i];
+ 
+        delete[] arr;
+    }
+
+    return 0;
+}

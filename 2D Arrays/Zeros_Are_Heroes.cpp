0a963d5115f1afdc8677f44aa4be114c2412e830
Subhayan Das <nips.subhayan@gmail.com>
2020-12-29 01:39:19 +0530

    29 Dec 2020

---
+++ 2D Arrays/Zeros_Are_Heroes.cpp
@@ -0,0 +1,47 @@
+//Given a 2D array, Count the number of zeros of the array.
+
+/*Enter your code here. Read input from STDIN. Print your output to STDOUT*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int count, count2;
+
+    cin >> count >> count2;
+    if (count > 0)
+    {
+        int **arr = new int*[count];
+        
+        for (int i = 0; i < count; i++)
+        arr[i] = new int[count2];
+ 
+        for (int i = 0; i < count; i++)
+            for (int j = 0; j < count2; j++)
+                cin>>arr[i][j];
+
+        int ctr = 0;
+
+        for (int i = 0; i < count; i++)
+        {
+            for (int j = 0; j < count2; j++)
+            {
+                if (arr[i][j]==0)
+                {
+                    ctr++;
+                }
+                
+            }
+            
+        }
+         
+        cout<<ctr;
+ 
+        for (int i = 0; i < count; i++)
+            delete[] arr[i];
+ 
+        delete[] arr;
+    }
+
+    return 0;
+}
